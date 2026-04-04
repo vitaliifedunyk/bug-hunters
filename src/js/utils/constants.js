1952a75e450db71ecc8b8f3ba0c30dbd6b56b84c
Alina Kozlyuk <talina787@icloud.com>
2026-04-04 13:36:07 +0300

    categories: categories api

---
+++ src/js/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_ENDPOINTS = {
+    CATEGORIES: '/categories',
+    FURNITURES: '/furnitures',
+    FURNITURE_BY_ID: (id) => `/furnitures/${id}`,
+    ORDERS: '/orders',
+    FEEDBACKS: '/feedbacks',
+}
