7dd8f7e652aba9e66f69eb98478bd9b4cce3da24
Onitrix <andriyproshak17@gmail.com>
2026-04-05 20:24:18 +0200

    re-do JS/Accordion

--- src/js/sections/faq/faq.js
+++ src/js/sections/faq/faq.js
@@ -1,19 +1,21 @@
-document.querySelectorAll(".ac").forEach((item) => {
-    const btn = item.querySelector(".ac-trigger");
-    const open = item.querySelector(".icon-open");
-    const close = item.querySelector(".icon-close");
-  
-    let isOpen = false;
-  
-    btn.addEventListener("click", () => {
-      isOpen = !isOpen;
-  
-      if (isOpen) {
-          close.style.display = "inline-block";
-          open.style.display = "none";
-      } else {
-          open.style.display = "inline-block";
-          close.style.display = "none"; 
-      }
-    });
-  });  
+import Accordion from 'accordion-js';
+
+let faqAccordionInstance;
+
+export function initFaq() {
+  const container = document.querySelector('.accordion-container');
+
+  if (!container) {
+    return;
+  }
+
+  if (faqAccordionInstance) {
+    faqAccordionInstance.destroy();
+  }
+
+  faqAccordionInstance = new Accordion(container, {
+    duration: 220,
+    showMultiple: false,
+    collapse: true,
+  });
+}
